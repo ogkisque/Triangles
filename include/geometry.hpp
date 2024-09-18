8b12113836611798c8a459c4e94503e2cdf315fb
Yaroslav Kuznetsov <asklater@bk.ru>
2024-09-18 23:24:08 +0300

    add c_tors and is_valid for each classes

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -4,43 +4,45 @@
 #pragma once
 
 namespace geometry {
-
     class point_t
     {
     public:
         double x_ = NAN; 
         double y_ = NAN;
+        double z_ = NAN;
 
         bool is_valid() const;
         void print() const;
         bool equal(const point_t &other_p) const;
         
-        point_t(const double x, const double y) : x_(x), y_(y) {}
+        point_t(const double x, const double y, const double z) : x_(x), y_(y), z_(z) {}
 
     }; // class point_t
 
 
     class line_t
     {
-        double a_ = -1, b_ = 1, c_ = 0;
-
     public:
+        double x0_ = NAN, y0_ = NAN, z0_ = NAN;
+        double a_  = NAN, b_  = NAN, c_  = NAN;
+    
         bool is_valid() const;
         void print() const; 
-        
 
         line_t(const point_t &p1, const point_t &p2);
-        line_t(const double a, const double b, const double c) : a_(a), b_(b), c_(c) {}
+        // line_t(const double a, const double b, const double c) : a_(a), b_(b), c_(c) {}
 
     }; // class line_t
 
     class triangle_t 
     {
-        point_t p1_, p2_, p3_;
     public:
+        point_t p1_, p2_, p3_;
+        line_t l1_, l2_, l3_;
+    
         bool is_valid() const;
 
-        triangle_t(const point_t &p1, const point_t &p2, const point_t &p3) : p1_(p1), p2_(p2), p3_(p3) {} 
+        triangle_t(const point_t &p1, const point_t &p2, const point_t &p3) : p1_(p1), p2_(p2), p3_(p3), l1_(p1, p2), l2_(p2, p3), l3_(p3, p1) {} 
     }; // 
 
 } // namespace geometry
--- src/geometry.cpp
+++ src/geometry.cpp
@@ -1,10 +1,18 @@
 #include "geometry.hpp"
 
+
+namespace {
+    bool equal(const double x, const double y)
+    {
+        return std::fabs(x - y) < std::numeric_limits<double>::epsilon();
+    }
+}
+
 namespace geometry{
     
         bool point_t::is_valid() const
         {
-            return (x_ != NAN && y_ != NAN);
+            return (x_ != NAN && y_ != NAN && z_ != NAN);
         }
         
         void point_t::print() const
@@ -14,12 +22,19 @@ namespace geometry{
 
         bool point_t::equal(const point_t &other) const
         {
-            return (this->x_ == other.x_ && this->y_ == other.y_); 
+            bool equal = std::fabs(this->x_ - other.x_) < std::numeric_limits<double>::epsilon();
+            equal &= std::fabs(this->y_ - other.y_) < std::numeric_limits<double>::epsilon();
+            equal &= std::fabs(this->z_ - other.z_) < std::numeric_limits<double>::epsilon();
+            return equal; 
         }
 
         bool line_t::is_valid() const 
         {
-            return a_ || b_ || c_;
+            bool is_valid = x0_ != NAN && y0_ != NAN && z0_ != NAN; 
+            is_valid = a_ != NAN && b_ != NAN && c_ != NAN;
+            is_valid &= a_ || b_ || c_;
+            
+            return is_valid;
         }
 
         void line_t::print() const 
@@ -27,16 +42,32 @@ namespace geometry{
             std::cout << "line:" << a_ << " * x + " << b_ << " y + " << c_ << " = 0" << std::endl;
         }
 
-        line_t::line_t(const point_t &p1, const point_t &p2)
-        {
-            double angle = std::atan((p2.y_ - p1.y_) / (p2.x_ - p1.x_));
-            double sin_angle = std::sin(angle);
-            double cos_angle = std::sqrt(1 - sin_angle * sin_angle);
+        line_t::line_t(const point_t &p1, const point_t &p2) :  x0_(p1.x_),        y0_(p1.y_),        z0_(p1.z_),
+                                                                a_(p2.x_ - p1.x_), b_(p2.y_ - p1.y_), c_(p2.z_ - p1.z_) {}
 
-            point_t normal_vector{-sin_angle, cos_angle};
-            a_ = -sin_angle;
-            b_ =  cos_angle;
-            c_ = -(p1.x_ * (-sin_angle) + p2.y_ * cos_angle);
+        bool triangle_t::is_valid() const
+        {
+            bool is_valid = l1_.is_valid() && l2_.is_valid() && l3_.line_t::is_valid(); 
+            
+            if (!is_valid)
+                return false;
+
+            double frac1 = 0, frac2 = 0, frac3 = 0;
+
+            
+            if (!equal(l2_.a_, 0))
+                frac1 = l1_.a_ / l2_.a_;
+                
+            if (!equal(l2_.b_, 0))
+                frac2 = l1_.b_ / l2_.b_;
+
+            if (!equal(l2_.c_, 0))
+                frac3 = l1_.c_ / l2_.c_;
+            
+            is_valid &= (frac1 == frac2 && frac2 == frac3);
+
+            return is_valid;
         }
+        
 
 } // namespace geometry
