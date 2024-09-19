ac6d0e337ae3f577ddab228f1df6ad313f950ab0
Arseniy Merzlyakov <merzliakov.ad@phystech.edu>
2024-09-19 18:24:59 +0300

    fixes, poin belongs to line

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -1,63 +1,66 @@
 #include <cmath>
 #include <iostream>
+#include <limits>
+#include <cassert>
+#include <variant>
 
 #pragma once
 
 namespace geometry {
-
     class point_t
     {
     public:
-        double x_ = NAN; 
-        double y_ = NAN;
-        double z_ = NAN;
+        double x_ = 0; 
+        double y_ = 0;
+        double z_ = 0;
 
-        bool is_valid() const;
-        void print() const;
-        bool equal(const point_t &other_p) const;
-        
+        void print()                            const;
+        bool operator==(const point_t& other_p) const;
+
         point_t(const double x, const double y, const double z) : x_(x), y_(y), z_(z) {}
-        point_t(const point_t& p) : x_(p.x_), y_(p.y_), z_(p.z_) {}
-
-        bool operator==(const point_t& other_p) const
-        {
-            return this->equal(other_p);
-        }
+        point_t(const point_t &point);
     }; // class point_t
 
     class line_t
     {
     public:
         point_t p1_, p2_;
-        double a_  = NAN, b_  = NAN, c_  = NAN;
+        double a_ = 0;
+        double b_ = 0;
+        double c_ = 0;
     
-        bool is_valid() const;
-        void print() const; 
+        bool    is_valid()      const;
+        void    print()         const; 
+        double  get_length()    const;
 
         line_t(const point_t &p1, const point_t &p2);
     }; // class line_t
 
-    class plane_t
-    {
-    public:
-        double a, b, c, d;
-
-        plane_t(const point_t p1, const point_t p2, const point_t p3);
-    }; // class plane_t
-
     class triangle_t 
     {
     public:
-        plane_t plane;
+        plane_t plane_;
         point_t p1_, p2_, p3_;
         line_t l1_, l2_, l3_;
+        int id_ = 0;
     
         bool is_valid() const;
 
-        triangle_t(const point_t &p1, const point_t &p2, const point_t &p3) : p1_(p1), p2_(p2), p3_(p3), l1_(p1, p2), l2_(p2, p3), l3_(p3, p1), plane(p1, p2, p3) {} 
+        triangle_t(const point_t &p1, const point_t &p2, const point_t &p3, int id);
     }; // class triangle_t
 
+    class plane_t
+    {
+    public:
+        double a_ = 0;
+        double b_ = 0;
+        double c_ = 0;
+        double d_ = 0;
+
+        plane_t(const point_t &p1, const point_t &p2, const point_t &p3);
+    }; // class plane_t
 
     bool point_belongs_to_line(const point_t& p, const line_t& l);
+
 
 } // namespace geometry
--- src/geometry.cpp
+++ src/geometry.cpp
@@ -1,105 +1,158 @@
 #include "geometry.hpp"
 
 
-namespace {
+namespace
+{
     bool equald(const double x, const double y)
     {
         return std::fabs(x - y) < std::numeric_limits<double>::epsilon();
     }
 }
 
-namespace geometry{
+namespace geometry
+{
 
-    bool point_belongs_to_line(const point_t& p, const line_t& l)
+//  General functions
+
+    bool point_belongs_to_line(const point_t& p, const line_t& l) // принадлежит ли точка отрезку
     {
-        ///TODO: not done
-        
-        double param = 0;
+        double param1 = 0;
+        double param2 = 0;
+        double cur_param = 0;
         
         if (!equald(l.a_, 0))
-            param = (p.x_ - l.p1_.x_) / l.a_;
+        {
+            param2 = (l.p2_.x_ - l.p1_.x_) / l.a_;
+            cur_param = (p.x_ - l.p1_.x_) / l.a_;
+        }
         else if (!equald(l.b_, 0))
-            param = (p.y_ - l.p1_.y_) / l.b_;
+        {
+            param2 = (l.p2_.y_ - l.p1_.y_) / l.b_;
+            cur_param = (p.y_ - l.p1_.y_) / l.b_;
+        }
         else if (!equald(l.c_, 0))
-            param = (p.z_ - l.p1_.z_) / l.c_;
+        {
+            param2 = (l.p2_.z_ - l.p1_.z_) / l.c_;
+            cur_param = (p.z_ - l.p1_.z_) / l.c_;
+        }
         else
-            // is not valid
+        {
+            assert(("Line is not valid", 0));
+        }
 
-        point_t temp_point{l.p1_.x_ + l.a_* param, l.p1_.y_ + l.b_ * param, l.p1_.z_ + l.c_ * param};
+        point_t temp_point{l.p1_.x_ + l.a_ * cur_param,
+                           l.p1_.y_ + l.b_ * cur_param,
+                           l.p1_.z_ + l.c_ * cur_param};
 
         if (!(temp_point == p))
             return false;
 
-        point_t p1 = l.p1_;
-        point_t p2 = l.p1_;
-        
-        if ((p.x_ > p1.x_ && p.x_ > p2.x_) || (p.x_ < p1.x_ && p.x_ < p2.x_))
-            return false;
-        if ((p.y_ > p1.y_ && p.y_ > p2.y_) || (p.y_ < p1.y_ && p.y_ < p2.y_))
-            return false;
-        if ((p.z_ > p1.z_ && p.z_ > p2.z_) || (p.z_ < p1.z_ && p.z_ < p2.z_))
-            return false;
-
-        return true;
+        return std::min(param1, param2) < cur_param < std::max(param1, param2);
     }
 
-    bool point_t::is_valid() const
-    {
-        return (x_ != NAN && y_ != NAN && z_ != NAN);
-    }
-    
+
+//  =========================== Point functions ===========================
+
+    point_t::point_t(const double x, const double y, const double z) : x_(x), y_(y), z_(z) {}
+    point_t::point_t(const point_t &point) : x_(point.x_), y_(point.y_), z_(point.z_) {}
+
     void point_t::print() const
     {
         std::cout << "( " << x_ << " ; " << y_ << " )" << std::endl;
     }
 
-    bool point_t::equal(const point_t &other) const
+    bool point_t::operator==(const point_t &other) const
     {
         return equald(this->x_, other.x_) && equald(this->y_, other.y_) && equald(this->z_, other.z_); 
     }
 
+
+//  =========================== Line functions ============================
+
+    line_t::line_t(const point_t &p1, const point_t &p2) :  
+        p1_(p1), p2_(p2),
+        a_(p2.x_ - p1.x_),
+        b_(p2.y_ - p1.y_),
+        c_(p2.z_ - p1.z_)
+        {}
+
     bool line_t::is_valid() const 
     {
-        bool is_valid = p1_.is_valid() && p2_.is_valid();
-        is_valid &= a_ != NAN && b_ != NAN && c_ != NAN;
-        is_valid &= a_ || b_ || c_;
+        bool is_valid = true;
+        is_valid &= (a_ != NAN && b_ != NAN && c_ != NAN);
+        is_valid &= (a_ || b_ || c_);
         
         return is_valid;
     }
 
     void line_t::print() const 
     {
-        std::cout << "line:" << a_ << " * x + " << b_ << " y + " << c_ << " = 0" << std::endl;
+        std::cout << "line:" << std::endl <<
+        "x = " << p1_.x_ << " + t * " << a_ << std::endl <<
+        "y = " << p1_.y_ << " + t * " << b_ << std::endl <<
+        "z = " << p1_.z_ << " + t * " << c_ << std::endl;
     }
 
-    line_t::line_t(const point_t &p1, const point_t &p2) :  p1_(p1), p2_(p2), a_(p2.x_ - p1.x_), b_(p2.y_ - p1.y_), c_(p2.z_ - p1.z_) {}
+    double line_t::get_length() const
+    {
+        return sqrt((p1_.x_ - p2_.x_) * (p1_.x_ - p2_.x_) + 
+                    (p1_.y_ - p2_.y_) * (p1_.y_ - p2_.y_) +
+                    (p1_.z_ - p2_.z_) * (p1_.z_ - p2_.z_));
+    }
+
+
+//  =========================== Triangle functions ===========================
+
+    triangle_t::triangle_t(const point_t &p1, const point_t &p2, const point_t &p3, int id) :
+            p1_(p1), p2_(p2), p3_(p3),
+            l1_(p1, p2), l2_(p2, p3), l3_(p3, p1),
+            plane_(p1, p2, p3), id_(id)
+            {}
 
     bool triangle_t::is_valid() const
     {
-        bool is_valid = l1_.is_valid() && l2_.is_valid() && l3_.line_t::is_valid(); 
+        bool is_valid = l1_.is_valid() && l2_.is_valid() && l3_.is_valid(); 
         
         if (!is_valid)
             return false;
 
-        double frac1 = 0, frac2 = 0, frac3 = 0;
+        double len1 = l1_.get_length();
+        double len2 = l2_.get_length();
+        double len3 = l3_.get_length();
 
-        if (!equald(l2_.a_, 0))
-            frac1 = l1_.a_ / l2_.a_;
-            
-        if (!equald(l2_.b_, 0))
-            frac2 = l1_.b_ / l2_.b_;
-
-        if (!equald(l2_.c_, 0))
-            frac3 = l1_.c_ / l2_.c_;
-        
-        is_valid &= (frac1 == frac2 && frac2 == frac3);
+        is_valid &= ((len1 + len2 > len3) & (len1 + len3 > len2) & (len2 + len3 > len1));
 
         return is_valid;
     }
 
-    plane_t::plane_t(const point_t p1, const point_t p2, const point_t p3)
+
+//  ============================ Plane functions ============================
+
+    plane_t::plane_t(const point_t &p1, const point_t &p2, const point_t &p3)
     {
-        ///TODO: c_tor plane
+        double x1 = p1.x_;
+        double y1 = p1.y_;
+        double z1 = p1.z_;
+        double delta_x2 = p2.x_ - p1.x_;
+        double delta_y2 = p2.y_ - p1.y_;
+        double delta_z2 = p2.z_ - p1.z_;
+        double delta_x3 = p3.x_ - p1.x_;
+        double delta_y3 = p3.y_ - p1.y_;
+        double delta_z3 = p3.z_ - p1.z_;
+
+        a_ = delta_y2 * delta_z3 - delta_y3 * delta_z2;
+        b_ = delta_z2 * delta_x3 - delta_z3 * delta_x2;
+        c_ = delta_y2 * delta_z3 - delta_y3 * delta_z2;
+        d_ = -x1 * a_ - y1 * b_ - z1 * c_;
     }
+
+//  ========================== Intersection functions ==========================
+    intersection::intersection(const point_t &point)
+    {
+        type_ = intersection::type::POINT;
+        point_ = point;
+    }
+
+    
 
 } // namespace geometry
