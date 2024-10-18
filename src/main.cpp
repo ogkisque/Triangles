3c35e71f57342150fb1fbfdcbc6d356f7226987c
Arseniy Merzlyakov <merzliakov.ad@phystech.edu>
2024-10-18 19:59:49 +0300

    add process of incorrect input

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,16 @@ namespace
     {
         size_t num_figs = 0;
         std::cin >> num_figs;
-        assert(std::cin.good());
+        if (!std::cin.good())
+            return 0;
 
         for (size_t i = 0; i < num_figs; i++)
         {
             double x1, x2, x3, y1, y2, y3, z1, z2, z3;
             std::cin >> x1 >> y1 >> z1 >> x2 >> y2 >> z2 >> x3 >> y3 >> z3; 
-            assert(std::cin.good());
+            if (!std::cin.good())
+                return 0;
+
             geometry::point_t point1{x1, y1, z1};
             geometry::point_t point2{x2, y2, z2};
             geometry::point_t point3{x3, y3, z3};
@@ -36,6 +39,12 @@ int main()
 {
     std::vector<geometry::figure_t> figs;
     size_t num_figs = input_figs(figs);
+
+    if (num_figs == 0)
+    {
+        std::cout << "INCORRECT INPUT" << std::endl;
+        return 1;
+    }
 
     std::set<size_t> intersect_figs_id;
     octotree::intersect_figs(figs, intersect_figs_id);
